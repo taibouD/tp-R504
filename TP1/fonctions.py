38667db2429358ce117268105c467f243c55f3f3
taibouD <taibou.diallo@univ-rouen.fr>
2023-09-06 17:26:38 +0200

    les fichiers fonctios

---
+++ TP1/fonctions.py
@@ -0,0 +1,8 @@
+def puissance (a,b):
+	if not type(var) is int:
+	   raise TypeError("Only integers are allowed")
+	return a** b
+
+
+
+
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,15 @@
-print("hello, world!")
+import fonctions as f
+while True:
+    # Saisie d'un nombre au clavier
+    a = float(input("Veuillez entrer un nombre a : "))
+    b = float(input("Veuillez entrer un nombre b : "))
+
+
+    # Calcul du carré
+    carre = a** b
+
+    # Affichage du résultat
+    print(f"Le carré de {a}**{b} est : {carre}")
+    res = f.puissance(a,b)
+    print res
+
