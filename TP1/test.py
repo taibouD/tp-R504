122f497c982357aaa53dfc8987f093c757f8e090
taibouD <taibou.diallo@univ-rouen.fr>
2023-09-28 15:50:25 +0200

    condition if

--- TP1/test.py
+++ TP1/test.py
@@ -6,5 +6,15 @@ def test_1 ( ) :
 	assert f.puissance ( 2 , 2 ) == 4
 	
 def test_2 ( ) :
-	assert f.puissance ( 1,1 ) == 1
-	assert f.puissance ( 2, 2 ) == 4
+	assert f.puissance ( 1 , 1 ) == 1
+	assert f.puissance ( 2, 1 ) == 2
+	
+def test_3 ( ) :
+  if f.puissance (0,-2)
+    return error
+    else 
+	assert f.puissance ( 0 , 0 ) == 0
+	assert f.puissance ( 1, -2 ) == 1
+	assert f.puissance ( 1 , 2 ) == 1
+	assert f.puissance ( 2 , 2 ) == 4
+	assert f.puissance ( 2, -2 ) == 1
