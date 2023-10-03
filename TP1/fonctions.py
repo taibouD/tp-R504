d828cce7dc8da6341f967663cb666d0cb8b3ec35
taibouD <taibou.diallo@univ-rouen.fr>
2023-10-03 09:26:31 +0200

    maj

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,10 +1,11 @@
 def puissance (a,b):
 	if not type(a) is int:
-	   raise TypeError("Only integers are allowed")
+		raise TypeError("Only integers are allowed")
 	if not type(b) is int:
-	   raise TypeError("Only integers are allowed")
-        if
-	   raise ArithmeticError("b negatif")
+		raise TypeError("Only integers are allowed")
+	   
+	if a == 0 and b<0:
+		raise ArithmeticError("b negatif")
 	   
 	return a** b
 
--- TP1/test.py
+++ TP1/test.py
@@ -9,14 +9,16 @@ def test_2 ( ) :
 	assert f.puissance ( 1 , 1 ) == 1
 	assert f.puissance ( 2, 1 ) == 2
 	
-def test_3 ( ) :
-	if f.puissance ( 0, -2 ):
-	    return error
+#def test_3 ( ) :
+#	if f.puissance ( 0, -2 ): 
+
+
 def test_zero_division():
 	with pytest.raises(ArithmeticError):
-		 #assert f.puissance (0 , -2)
-		 assert f.puissance ( 0 , 0 ) == 0
+		 assert f.puissance (0 , -2)
+
+		 assert f.puissance ( 0 , 0 ) == 1
 		 assert f.puissance ( 1, -2 ) == 1
 		 assert f.puissance ( 1 , 2 ) == 1
 		 assert f.puissance ( 2 , 2 ) == 4
-		 assert f.puissance ( 2, -2 ) == 1
+		 assert f.puissance ( 2, -2 ) == 0.25
