1f0d6cb28f27d61e4f767acc4956d6ecce74fe60
user <user@debian.root>
2023-10-25 11:28:38 -0400

    suite du tp4

--- tp4/create_network.sh
+++ tp4/create_network.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-docker run −−rm −d \
-	−p 3307 : 3306 \
-	−v vol−sql−demo : / var/lib/mysql \
-	−−name tp4−sql \
-	−−env MYSQL_ROOT_PASSWORD=foo \
-	−−network net−tp4 \
-	mysql
+docker network create net-tp4
+if [ $? != ]
+then 
+	echo "erreur à l'étape ***"
+	exit
+fi
